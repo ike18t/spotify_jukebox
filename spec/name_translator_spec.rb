e2158d0627787bcb26318fd9a78b206e3365bc8e
Isaac Datlof <ike18t@gmail.com>
2014-01-20 10:32:57 -0500

    stubbing cache action in name translator

--- spec/name_translator_spec.rb
+++ spec/name_translator_spec.rb
@@ -4,11 +4,13 @@ describe NameTranslator do
   context 'get_for' do
     it 'should return name from cache if it is cached' do
       CacheHandler.stubs(:get_user_mappings).returns({:a => 'a', :b => 'b'})
+      CacheHandler.stubs(:cache_user_mappings!)
       NameTranslator.get_for(:a).should eq('a')
     end
 
     it 'should scrape name if not cached' do
       CacheHandler.stubs(:get_user_mappings).returns({:a => 'a', :b => 'b'})
+      CacheHandler.stubs(:cache_user_mappings!)
       SpotifyScraper.stubs(:name_from_spotify_id).with(:c).returns('c')
       NameTranslator.get_for(:c).should eq('c')
     end
