ff92634eaf4f2d5cc576815e79da12f17812f36c
Isaac Datlof <ike18t@gmail.com>
2014-05-05 15:18:08 -0400

    handle scraping a user that has no image

--- app/spotify_scraper.rb
+++ app/spotify_scraper.rb
@@ -5,7 +5,8 @@ module SpotifyScraper
   def self.name_and_image_from_spotify_id(spotify_id)
     doc = Nokogiri::HTML(open("http://open.spotify.com/user/#{spotify_id}"))
     name = doc.css("meta[property='og:title']").first['content']
-    image_url = doc.css("meta[property='og:image']").first['content']
+    doc_image = doc.css("meta[property='og:image']")
+    image_url = doc_image.empty? ? nil : doc_image.first['content']
     { :name => name, :image_url => image_url }
   end
 end
---
+++ spec/spotify_scraper_spec.rb
@@ -0,0 +1,30 @@
+require_relative 'spec_helper'
+
+describe SpotifyScraper do
+  context 'name_and_image_from_spotify_id' do
+    it 'should set the image_url if it is found' do
+      str = <<-HTML
+        <head>
+          <meta property="og:title" content="idatlof">
+          <meta property="og:image" content="http://google.com/ike.jpg">
+        </head>
+      HTML
+      SpotifyScraper.stubs(:open).returns(str)
+      name_and_url = SpotifyScraper.name_and_image_from_spotify_id 123
+      name_and_url[:name].should eq('idatlof')
+      name_and_url[:image_url].should eq('http://google.com/ike.jpg')
+    end
+
+    it 'should set the image_url to nil if it is not found' do
+      str = <<-HTML
+        <head>
+          <meta property="og:title" content="idatlof">
+        </head>
+      HTML
+      SpotifyScraper.stubs(:open).returns(str)
+      name_and_url = SpotifyScraper.name_and_image_from_spotify_id 123
+      name_and_url[:name].should eq('idatlof')
+      name_and_url[:image_url].should eq(nil)
+    end
+  end
+end
