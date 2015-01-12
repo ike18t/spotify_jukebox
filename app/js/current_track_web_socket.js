37962e49e922d312c1534cad2c3959dcbca9d185
Isaac Datlof <ike18t@gmail.com>
2015-01-12 17:25:29 -0500

    refactor enabled playlist/user js handling

--- app/js/current_track_web_socket.js
+++ app/js/current_track_web_socket.js
@@ -8,17 +8,9 @@ var currentTrackWebSocket = function() {
     $container.find('#user-data').find('#name').text("Brought to you by " + data.user.name);
   };
 
-  var updateEnabledUsers = function(data) {
-    $('.users li').each(function(idx, item) {
-      var $item = $(item);
-      data.indexOf(item.id) >= 0 ? $item.addClass('enabled', 500) : $item.removeClass('enabled', 500);
-    });
-  };
-
-  var updateEnabledPlaylists = function(data) {
-    $('li.playlist').each(function(idx, item) {
-      var $item = $(item);
-      data.indexOf(item.id) >= 0 ? $item.addClass('active', 500) : $item.removeClass('active', 500);
+  var updateEnabled = function(selector, data) {
+    $(selector).each(function(idx, item) {
+      $(item).toggleClass('disabled', data.indexOf(item.id) === -1);
     });
   };
 
@@ -61,10 +53,10 @@ var currentTrackWebSocket = function() {
         updateTrackInfo(json_message.current_track);
       }
       if (json_message.hasOwnProperty('enabled_users')) {
-        updateEnabledUsers(json_message.enabled_users);
+        updateEnabled('li.user', json_message.enabled_users);
       }
       if (json_message.hasOwnProperty('enabled_playlists')) {
-        updateEnabledPlaylists(json_message.enabled_playlists);
+        updateEnabled('li.playlist', json_message.enabled_playlists);
       }
       if (json_message.hasOwnProperty('play_status')) {
         updatePlayStatus(json_message.play_status);
