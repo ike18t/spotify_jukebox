7520042a8a029052c720739d8510c87f7dbb1a10
Isaac Datlof <ike18t@gmail.com>
2016-08-26 16:42:51 -0400

    Add some negative case tests to web socket service

--- frontend/spec/services/web_socket_service.spec.ts
+++ frontend/spec/services/web_socket_service.spec.ts
@@ -11,5 +11,23 @@ describe('WebSocketService', () => {
       socket.onmessage(new MessageEvent('stub', { data: JSON.stringify({ current_track: 'new value' }) }));
       expect(value).toEqual('new value');
     });
+
+    it('should not fire the callback if the event is valid but not for the registered listener', () => {
+      let socket = new WebSocket('ws://mock');
+      let service = new WebSocketService(socket);
+      let value = 'original value';
+      service.registerListener('current_track', (data: any) => value = data);
+      socket.onmessage(new MessageEvent('stub', { data: JSON.stringify({ foo: 'new value' }) }));
+      expect(value).toEqual('original value');
+    });
+
+    it('should not fire the callback if the event is invalid', () => {
+      let socket = new WebSocket('ws://mock');
+      let service = new WebSocketService(socket);
+      let value = 'original value';
+      service.registerListener('current_track', (data: any) => value = data);
+      socket.onmessage(new MessageEvent('stub', { data: '{}' }));
+      expect(value).toEqual('original value');
+    });
   });
 });
